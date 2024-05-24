2b79a6be0498615d6f8d63f3f0d59c93e20d5440
Gabriel Gilder <gabriel@gabrielgilder.com>
2024-05-24 11:09:46 -0700

    Improve logging

--- src/action.ts
+++ src/action.ts
@@ -56,13 +56,27 @@ export async function play(): Promise<void> {
       // lcov default
       var parsedCov = await parseLCov(COVERAGE_FILE_PATH)
     }
-    core.info('Parsing done')
+    // Sum up lines.found for each entry in parsedCov
+    const totalLines = parsedCov.reduce(
+      (acc, entry) => acc + entry.lines.found,
+      0
+    )
+    const coveredLines = parsedCov.reduce(
+      (acc, entry) => acc + entry.lines.hit,
+      0
+    )
+    core.info(
+      'Parsing done. ${parsedCov.length} files parsed. Total lines: ${totalLines}. Covered lines: ${coveredLines}'
+    )
 
     // 2. Filter Coverage By File Name
     const coverageByFile = filterCoverageByFile(parsedCov)
     core.info('Filter done')
     if (debugOpts['coverage']) {
-      core.info(`Coverage: ${JSON.stringify(coverageByFile)}`)
+      core.info(`Coverage:`)
+      for (const item of coverageByFile) {
+        core.info(JSON.stringify(item))
+      }
     }
     const githubUtil = new GithubUtil(GITHUB_TOKEN)
 
